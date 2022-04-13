d9aeffe0a51e636a08941a03ba4117be54612cb6
lei <xionglei1614@163.com>
2022-04-14 00:53:08 +0800

    add(zuo): 初始化zuo相关项目代码

---
+++ src/main/java/com/annamaple/zuo/Main.java
@@ -0,0 +1,13 @@
+package com.annamaple.zuo;
+
+import cn.hutool.core.lang.Console;
+
+/**
+ * 这个包下的代码是左神算法可的题的代码
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Console.log(Main.class);
+    }
+}
