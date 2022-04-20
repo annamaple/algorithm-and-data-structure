4e131f39e580a321af832bea68a3f7745fb7bd22
熊雷 <xionglei@jiuqi.com.cn>
2022-04-20 10:34:09 +0800

    add(base): 增加进制在Java代码中的符号表示

---
+++ src/main/java/com/annamaple/base/Binary.java
@@ -0,0 +1,21 @@
+package com.annamaple.base;
+
+/**
+ * 二进制
+ * @author xionglei
+ * @create 2022-04-20 10:29
+ */
+public class Binary {
+
+    public static void main(String[] args) {
+        // 2进制2
+        int a = 0b10;
+        // 八进制 少用, 易混淆
+        int b = 010;
+        // 16进制
+        int c = 0x10;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
